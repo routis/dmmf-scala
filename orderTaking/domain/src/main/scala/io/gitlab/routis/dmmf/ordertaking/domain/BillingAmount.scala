9df23691c923bc1a34f9639368d546ebcba8f192
babis <haralampos.routis@intrasoft-intl.com>
2022-09-08 15:02:31 +0300

    Simplified Assertions for Price and BillingAmount

--- orderTaking/domain/src/main/scala/io/gitlab/routis/dmmf/ordertaking/domain/BillingAmount.scala
+++ orderTaking/domain/src/main/scala/io/gitlab/routis/dmmf/ordertaking/domain/BillingAmount.scala
@@ -1,15 +1,21 @@
 package io.gitlab.routis.dmmf.ordertaking.domain
 
 import zio.NonEmptyChunk
-import zio.prelude.{ Assertion, Subtype, Validation }
+import zio.prelude.{ Assertion, Identity, Subtype, Validation }
 import Assertion.*
+import zio.prelude.newtypes.Sum
 
 object BillingAmount extends Subtype[Money]:
 
-  override inline def assertion: Assertion[Money] =
-    greaterThanOrEqualTo(Money(0)) && lessThanOrEqualTo(Money(10000))
+  override inline def assertion: Assertion[Money] = between(Money(0), Money(10000))
 
   def total(prices: NonEmptyChunk[Price]): Validation[String, BillingAmount] =
     import zio.prelude.*
-    import Money.MoneyAdditionIsIdentity
-    make(prices.foldMap[Money](identity))
+    def sum(ms: NonEmptyChunk[Money]): Money =
+      import Money.MoneyAdditionIsIdentity
+      ms.reduceIdentity
+    // Price type is not an Identity (aka Monoid) on addition because of it
+    // could be the case that when adding two prices you may exceed the limit
+    // Thus, to sum Prices (which is a sub-class of Money) you have to sum Money
+    // which has an Identity on addition
+    make(sum(prices))
--- orderTaking/domain/src/main/scala/io/gitlab/routis/dmmf/ordertaking/domain/Price.scala
+++ orderTaking/domain/src/main/scala/io/gitlab/routis/dmmf/ordertaking/domain/Price.scala
@@ -6,15 +6,10 @@ import Assertion.*
 import scala.annotation.targetName
 
 object Price extends Subtype[Money]:
-  override inline def assertion: Assertion[Money] =
-    greaterThanOrEqualTo(Money.zero) && lessThanOrEqualTo(Money(1000))
-
-  def make(amount: BigDecimal): Validation[String, Price.Type] =
-    Money.make(amount).flatMap(make)
-  inline def apply(inline amount: BigDecimal): Price =
-    wrap(Money(amount))
+  override inline def assertion: Assertion[Money]         = between(Money(0), Money(1000))
+  def make(amount: BigDecimal): Validation[String, Price] = Money.make(amount).flatMap(make)
+  inline def apply(inline amount: BigDecimal): Price      = wrap(Money(amount))
 
   extension (self: Price)
     @targetName("*")
-    def *(times: Long): Validation[String, Price.Type] =
-      make(unwrap(self) * times)
+    def *(times: Long): Validation[String, Price] = make(unwrap(self) * times)
