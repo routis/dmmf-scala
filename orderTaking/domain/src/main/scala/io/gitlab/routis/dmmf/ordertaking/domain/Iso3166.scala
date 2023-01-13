f7bd23013e96ffa62b87d5ac737dbb482c038e3c
Routis Babis <haralampos.routis@intrasoft-intl.com>
2023-01-13 10:11:17 +0200

    Iso3166 country code implemented as NewtypeCustom

--- orderTaking/domain/src/main/scala/io/gitlab/routis/dmmf/ordertaking/domain/Iso3166.scala
+++ orderTaking/domain/src/main/scala/io/gitlab/routis/dmmf/ordertaking/domain/Iso3166.scala
@@ -1,29 +1,22 @@
 package io.gitlab.routis.dmmf.ordertaking.domain
 
-import java.util.Locale
 import zio.prelude.*
-import Assertion.*
+import zio.prelude.Assertion.*
+
+import java.util.Locale
+import java.util.Locale.getISOCountries
 
 object Iso3166:
 
-  object Part1Alpha2 extends Subtype[String]:
-    override inline def assertion: Assertion[String] =
-      hasLength(equalTo(2)) && equalToOneOf(getISOCountries(Locale.IsoCountryCode.PART1_ALPHA2))
+  object Part1Alpha2 extends NewtypeCustom[String]:
+    protected def validate(value: String): Either[AssertionError, Unit] =
+      Part1Alpha2Validator.validate(value)
 
-  object Part1Alpha3 extends Subtype[String]:
-    override inline def assertion: Assertion[String] =
-      hasLength(equalTo(3)) && equalToOneOf(getISOCountries(Locale.IsoCountryCode.PART1_ALPHA3))
+    inline def validateInline(inline value: String): Unit =
+      ${ Part1Alpha2Validator.validateInlineImpl('{ value }) }
 
-  private def equalToOneOf[A](as: Iterable[A]): Assertion[A] =
-    given orOfAssertionIsIdentity[A1]: Identity[Assertion[A1]] with
-      override def identity: Assertion[A1]                                          = Assertion.never
-      override def combine(l: => Assertion[A1], r: => Assertion[A1]): Assertion[A1] = l || r
-    as.foldMap(equalTo)
-
-  private def getISOCountries(isoType: Locale.IsoCountryCode): Iterable[String] =
-    import scala.jdk.CollectionConverters.*
-    Locale.getISOCountries(isoType).asScala
-
-  def main(args: Array[String]): Unit =
-    val gr = Part1Alpha2.makeAll(List("GR", "DE"))
-    println(gr)
+  object Part1Alpha2Validator
+      extends zio.prelude.Validator[String](str =>
+        if str.length == 2 && Locale.getISOCountries(Locale.IsoCountryCode.PART1_ALPHA2).contains(str) then Right(())
+        else Left(AssertionError.failure("isIso3166Part1Alpha2"))
+      )
