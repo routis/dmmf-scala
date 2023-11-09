7c9d12c443c1c729344e07ace830290133e56e2c
Scala Steward <scala_steward@virtuslab.com>
2023-11-09 21:45:17 +0000

    Update mariadb-java-client to 2.7.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
 
     val zio = "2.0.18"
     // val zio_quill    = "3.12.0.Beta1.7"
-    val mariadb   = "2.7.10"
+    val mariadb   = "2.7.11"
     val prelude   = "1.0.0-RC21"
     val jodaMoney = "1.0.3"
     val javafaker = "1.0.2"
